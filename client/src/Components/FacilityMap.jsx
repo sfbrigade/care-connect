3457c40d94654ec49334ae690db8919a34554907
Toby Negrin <tnegrin@pobox.com>
2025-11-09 08:40:17 -0800

    Basic map2 (#29)
    * removed auth checks; added posthog
    
    * working database
    
    * map basics
    
    * more maps
    
    * fixed CI error
    
    * forced to use static list of clinics instead of db for testing
    
    * Fixed for prod errors

--- client/src/Components/FacilityMap.jsx
+++ client/src/Components/FacilityMap.jsx
@@ -1,53 +1,45 @@
-import { useMemo } from 'react';
-
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
-
 import 'leaflet/dist/leaflet.css';
-
-const markerIcon = new L.Icon({
-  iconUrl: new URL('leaflet/dist/images/marker-icon.png', import.meta.url).href,
-  iconRetinaUrl: new URL('leaflet/dist/images/marker-icon-2x.png', import.meta.url).href,
-  shadowUrl: new URL('leaflet/dist/images/marker-shadow.png', import.meta.url).href,
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowAnchor: [12, 41],
-  iconSize: [25, 41],
-  shadowSize: [41, 41],
-});
 
 const DEFAULT_CENTER = [37.7749, -122.4194]; // San Francisco
 const DEFAULT_ZOOM = 12;
 
 function FacilityMap ({ facilities }) {
-  const facilityMarkers = useMemo(() => {
-    return facilities
-      .map((facility) => ({
-        ...facility,
-        latitude: Number(facility.latitude),
-        longitude: Number(facility.longitude),
-      }))
-      .filter((facility) => Number.isFinite(facility.latitude) && Number.isFinite(facility.longitude));
-  }, [facilities]);
-
-  const center = useMemo(() => {
-    if (!facilityMarkers.length) {
-      return DEFAULT_CENTER;
-    }
-    const [latSum, lngSum] = facilities.reduce(
-      (acc, facility) => {
-        acc[0] += Number(facility.latitude);
-        acc[1] += Number(facility.longitude);
-        return acc;
-      },
-      [0, 0]
-    );
-    return [latSum / facilities.length, lngSum / facilities.length];
-  }, [facilities]);
-
   if (typeof window === 'undefined') {
     return null;
   }
+
+  const L = require('leaflet');
+  const { MapContainer, Marker, Popup, TileLayer } = require('react-leaflet');
+
+  const facilityMarkers = facilities
+    .map((facility) => ({
+      ...facility,
+      latitude: Number(facility.latitude),
+      longitude: Number(facility.longitude),
+    }))
+    .filter((facility) => Number.isFinite(facility.latitude) && Number.isFinite(facility.longitude));
+
+  const markerIcon = new L.Icon({
+    iconUrl: new URL('leaflet/dist/images/marker-icon.png', import.meta.url).href,
+    iconRetinaUrl: new URL('leaflet/dist/images/marker-icon-2x.png', import.meta.url).href,
+    shadowUrl: new URL('leaflet/dist/images/marker-shadow.png', import.meta.url).href,
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowAnchor: [12, 41],
+    iconSize: [25, 41],
+    shadowSize: [41, 41],
+  });
+
+  const center = facilityMarkers.length
+    ? facilityMarkers.reduce(
+      (acc, facility, index, array) => {
+        acc[0] += facility.latitude / array.length;
+        acc[1] += facility.longitude / array.length;
+        return acc;
+      },
+      [0, 0]
+    )
+    : DEFAULT_CENTER;
 
   return (
     <MapContainer
