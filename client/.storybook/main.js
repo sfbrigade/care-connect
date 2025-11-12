74aaecbe14812f3a3eb096e9eee63149640db98c
Francis Li <mail@francisli.com>
2025-11-11 17:37:33 -0800

    Update storybook config

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -21,7 +21,8 @@ const config = {
     getAbsolutePath('@storybook/addon-docs'),
     getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-a11y'),
-    getAbsolutePath('@storybook/addon-vitest')
+    getAbsolutePath('@storybook/addon-vitest'),
+    getAbsolutePath('@vueless/storybook-dark-mode')
   ],
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
---
+++ client/.storybook/preview.jsx
@@ -0,0 +1,56 @@
+import '@mantine/core/styles.css';
+import { useEffect } from 'react';
+import { addons } from 'storybook/preview-api';
+import { DARK_MODE_EVENT_NAME } from '@vueless/storybook-dark-mode';
+import {
+  MantineProvider,
+  useMantineColorScheme,
+} from '@mantine/core';
+// theme.ts file from previous step
+import AppTheme from '../src/AppTheme';
+
+const channel = addons.getChannel();
+
+function ColorSchemeWrapper ({
+  children,
+}) {
+  const { setColorScheme } = useMantineColorScheme();
+  const handleColorScheme = (value) =>
+    setColorScheme(value ? 'dark' : 'light');
+
+  useEffect(() => {
+    channel.on(DARK_MODE_EVENT_NAME, handleColorScheme);
+    return () => channel.off(DARK_MODE_EVENT_NAME, handleColorScheme);
+  }, [channel]);
+
+  return <>{children}</>;
+}
+
+/** @type { import('@storybook/react-vite').Preview } */
+const preview = {
+  decorators: [
+    (renderStory) => (
+      <ColorSchemeWrapper>{renderStory()}</ColorSchemeWrapper>
+    ),
+    (renderStory) => (
+      <MantineProvider theme={AppTheme}>{renderStory()}</MantineProvider>
+    ),
+  ],
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: 'todo'
+    }
+  },
+};
+
+export default preview;
