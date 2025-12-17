62a9939ef617bf13a55fe40e7a35d8c41877a4c0
tnegrin <tnegrin@pobox.com>
2025-12-17 10:42:54 -0800

    fixed client tests lint

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig(({ command, ssrBuild, mode }) => {
