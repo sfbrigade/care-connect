010f31600b01f12e2640e93f3e3743f9b7bb0418
Francis Li <mail@francisli.com>
2026-01-01 14:42:54 -0800

    Fix hold actions export

--- client/src/lesc/hooks/useHoldActions.js
+++ client/src/lesc/hooks/useHoldActions.js
@@ -136,6 +136,7 @@ export function useHoldActions ({ invalidateQueries = [], onCancelSuccess } = {}
     cancelModalOpened,
     qrModalOpened,
     selectedHold,
+    setSelectedHold,
 
     // Handlers
     handleCancel,
