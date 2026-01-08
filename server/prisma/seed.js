2036ae28d8ebd36234b5bb7f055d2d0025adc928
Francis Li <mail@francisli.com>
2026-01-07 21:25:17 -0800

    Add more development seeds

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -2,10 +2,14 @@ import '../config.js';
 import prisma from './client.js';
 import seedFacilities from './seeds/facilities.js';
 import seedServiceTypes from './seeds/serviceTypes.js';
+import seedUsers from './seeds/users.js';
+import seedResetCenter from './seeds/resetCenter.js';
 
 try {
   await seedFacilities(prisma);
   await seedServiceTypes(prisma);
+  await seedUsers(prisma);
+  await seedResetCenter(prisma);
 } catch (error) {
   console.error('Error seeding:', error);
   process.exit(1);
