db34a485e2c595fc592ecba9a1106eabf35f8f6d
Francis Li <mail@francisli.com>
2026-01-21 11:15:15 -0800

    Code cleanup

--- client/src/lesc/routes/LESCRoutes.jsx
+++ client/src/lesc/routes/LESCRoutes.jsx
@@ -2,7 +2,6 @@ import { Routes, Route, Navigate } from 'react-router';
 
 import Holds from '../components/Holds';
 import CheckIn from '../components/CheckIn';
-import ClientView from '../components/ClientView';
 import IncidentForm from '../components/IncidentForm';
 import SubjectForm from '../components/SubjectForm';
 import Deflection from '../components/Deflection';
@@ -19,8 +18,6 @@ function LESCRoutes () {
       <Route path='holds' element={<Holds />} />
       <Route path='checkin/:holdId' element={<CheckIn />} />
       <Route path='checkin' element={<CheckIn />} />
-      <Route path='client/:clientId' element={<ClientView />} />
-      <Route path='client' element={<ClientView />} />
       <Route path='incident' element={<IncidentForm />} />
       <Route path='' element={<Navigate to='holds' />} />
     </Routes>
