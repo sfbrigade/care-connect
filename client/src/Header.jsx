a67573e305abbbd5567250efc0935821a768a080
Frances Cheng <francesvcheng@gmail.com>
2026-01-27 23:54:44 -0800

    Changed link text in menu

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,14 +1,11 @@
 import { Link, NavLink } from 'react-router';
 import { Anchor, Avatar, Burger, Box, Container, Group, Menu, Text, Title } from '@mantine/core';
-// import { IconMessages } from '@tabler/icons-react';
 import {
+  IconHome,
+  IconClipboardList,
   IconMessages,
-  IconSettings,
-  IconSearch,
-  IconPhoto,
-  IconMessageCircle,
-  IconTrash,
-  IconArrowsLeftRight,
+  IconBug,
+  IconLogout
 } from '@tabler/icons-react';
 import { useAuthContext } from '@/AuthContext';
 import { useFacilityContext } from '@/FacilityContext';
@@ -73,45 +70,24 @@ function Header ({ opened, close, toggle, logout }) {
           {user &&
           <Menu shadow="md" width={200}>
             <Menu.Target>
-              {/* <Button>Toggle menu</Button> */}
-                <Burger opened={opened} onClick={toggle} />
+              <Burger opened={opened} onClick={toggle} />
             </Menu.Target>
 
             <Menu.Dropdown>
-              <Menu.Label>Application</Menu.Label>
-              <Menu.Item leftSection={<IconSettings size={14} />}>
-                Settings
+              <Menu.Item leftSection={<IconHome size={14} />}>
+                Home
               </Menu.Item>
-              <Menu.Item leftSection={<IconMessageCircle size={14} />}>
-                Messages
+                <Menu.Item leftSection={<IconClipboardList size={14} />}>
+                Profile
               </Menu.Item>
-              <Menu.Item leftSection={<IconPhoto size={14} />}>
-                Gallery
-              </Menu.Item>
-              <Menu.Item
-                leftSection={<IconSearch size={14} />}
-                rightSection={
-                  <Text size="xs" c="dimmed">
-                    ⌘K
-                  </Text>
-                }
-              >
-                Search
-              </Menu.Item>
-
-              <Menu.Divider />
-
-              <Menu.Label>Danger zone</Menu.Label>
-              <Menu.Item
-                leftSection={<IconArrowsLeftRight size={14} />}
-              >
-                Transfer my data
+              <Menu.Item leftSection={<IconBug size={14} />}>
+                Report a Bug
               </Menu.Item>
               <Menu.Item
                 color="red"
-                leftSection={<IconTrash size={14} />}
+                  leftSection={<IconLogout size={14} />}
               >
-                Delete my account
+                Logout
               </Menu.Item>
             </Menu.Dropdown>
           </Menu>
--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -14,31 +14,31 @@ function Navbar ({ close, logout }) {
   const { user } = useAuthContext();
 
   return (
-    // <Stack gap='md'>
-    //   <MantineNavLink component={NavLink} to='/' onClick={close} label='Home' />
-    //   {!user && (
-    //     <MantineNavLink component={NavLink} to='/login' onClick={close} label='Login' />
-    //   )}
-    //   {user && (
-    //     <>
-    //       {user?.isAdmin && (
-    //         <>
-    //           <Divider />
-    //           <MantineNavLink component={NavLink} to='/admin/facilities' onClick={close} label='Facilities' />
-    //           <MantineNavLink component={NavLink} to='/admin/facility-status-reasons' onClick={close} label='Facility Status Reasons' />
-    //           <MantineNavLink component={NavLink} to='/admin/organizations' onClick={close} label='Organizations' />
-    //           <MantineNavLink component={NavLink} to='/admin/invites' onClick={close} label='Invites' />
-    //           <MantineNavLink component={NavLink} to='/admin/users' onClick={close} label='Users' />
-    //         </>
-    //       )}
-    //       <Divider />
-    //       <MantineNavLink component={NavLink} to='/account' onClick={close} label='Account' />
-    //       <MantineNavLink component={NavLink} to='/logout' onClick={logout} label='Log out' />
-    //       <Divider />
-    //       <MantineNavLink component={NavLink} to='/feedback' onClick={close} label='Feedback' />
-    //     </>
-    //   )}
-    // </Stack>
+    <Stack gap='md'>
+      <MantineNavLink component={NavLink} to='/' onClick={close} label='Home' />
+      {!user && (
+        <MantineNavLink component={NavLink} to='/login' onClick={close} label='Login' />
+      )}
+      {user && (
+        <>
+          {user?.isAdmin && (
+            <>
+              <Divider />
+              <MantineNavLink component={NavLink} to='/admin/facilities' onClick={close} label='Facilities' />
+              <MantineNavLink component={NavLink} to='/admin/facility-status-reasons' onClick={close} label='Facility Status Reasons' />
+              <MantineNavLink component={NavLink} to='/admin/organizations' onClick={close} label='Organizations' />
+              <MantineNavLink component={NavLink} to='/admin/invites' onClick={close} label='Invites' />
+              <MantineNavLink component={NavLink} to='/admin/users' onClick={close} label='Users' />
+            </>
+          )}
+          <Divider />
+          <MantineNavLink component={NavLink} to='/account' onClick={close} label='Account' />
+          <MantineNavLink component={NavLink} to='/logout' onClick={logout} label='Log out' />
+          <Divider />
+          <MantineNavLink component={NavLink} to='/feedback' onClick={close} label='Feedback' />
+        </>
+      )}
+    </Stack>
   );
 }
 
