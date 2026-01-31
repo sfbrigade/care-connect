d2909ca0df95245664eaa7d2edeab7f0d0b8e27e
Francis Li <mail@francisli.com>
2026-01-30 19:07:52 -0800

    Hold expiration refactor

--- server/prisma/client.js
+++ server/prisma/client.js
@@ -1,4 +1,4 @@
-import { Prisma, PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient, HoldStatusEnum } from '@prisma/client';
 
 const prisma = new PrismaClient({
   datasourceUrl: process.env.DATABASE_URL
@@ -15,6 +15,85 @@ const prisma = new PrismaClient({
           result = await tx.$queryRaw`SELECT * FROM "BedType" WHERE "id" = ${id}::uuid FOR UPDATE`;
           return result.length > 0 ? result[0] : null;
         }
+      }
+    },
+    deflection: {
+      async expire (now = new Date()) {
+        const bedTypeIds = (await prisma.deflection.findMany({
+          distinct: ['bedTypeId'],
+          select: {
+            bedTypeId: true,
+          },
+          where: {
+            status: HoldStatusEnum.ACTIVE,
+            expiresAt: {
+              lte: now,
+            },
+          },
+        })).map((deflection) => deflection.bedTypeId);
+        return Promise.all(bedTypeIds.map((bedTypeId) => {
+          return prisma.$transaction(async (tx) => {
+            const bedType = await tx.bedType.findByIdForUpdate(tx, bedTypeId);
+            const deflections = await tx.deflection.findMany({
+              where: {
+                bedTypeId,
+                status: HoldStatusEnum.ACTIVE,
+                expiresAt: {
+                  lte: now,
+                },
+              },
+            });
+            const deflectionUpdates = deflections.map((deflection) => ({
+              deflectionId: deflection.id,
+              status: HoldStatusEnum.EXPIRED,
+              updatedById: deflection.createdById,
+              updatedAt: now,
+            }));
+            await tx.deflectionUpdate.createMany({
+              data: deflectionUpdates,
+            });
+            await tx.deflection.updateMany({
+              where: {
+                id: {
+                  in: deflections.map((deflection) => deflection.id),
+                },
+              },
+              data: {
+                status: HoldStatusEnum.EXPIRED,
+                updatedAt: now,
+              },
+            });
+            const bedTypeUpdate = await tx.bedTypeUpdate.create({
+              data: {
+                bedTypeId: bedType.id,
+                facilityId: bedType.facilityId,
+                capacity: bedType.capacity,
+                unavailableUnoccupied: bedType.unavailableUnoccupied,
+                unavailableOccupied: bedType.unavailableOccupied,
+                occupied: bedType.occupied,
+                holds: bedType.holds - deflections.length,
+                available: bedType.available + deflections.length,
+                updateMethod: 'API',
+                updatedById: bedType.createdById,
+              },
+            });
+            await tx.bedType.update({
+              where: {
+                id: bedType.id,
+              },
+              data: {
+                capacity: bedTypeUpdate.capacity,
+                unavailableUnoccupied: bedTypeUpdate.unavailableUnoccupied,
+                unavailableOccupied: bedTypeUpdate.unavailableOccupied,
+                occupied: bedTypeUpdate.occupied,
+                holds: bedTypeUpdate.holds,
+                available: bedTypeUpdate.available,
+                updateMethod: bedTypeUpdate.updateMethod,
+                updatedById: bedTypeUpdate.updatedById,
+              },
+            });
+          });
+        }));
       }
     },
     $allModels: {
--- server/routes/api/incidents/extend.js
+++ server/routes/api/incidents/extend.js
@@ -3,7 +3,6 @@ import { z } from 'zod';
 
 import Deflection from '#models/deflection.js';
 import PropertyPhoto from '#models/propertyPhoto.js';
-import { autoExpireHolds } from '#lib/lesc/holds.js';
 
 export default async function (fastify, opts) {
   fastify.patch('/:id/extend',
@@ -34,7 +33,7 @@ export default async function (fastify, opts) {
         return reply.code(StatusCodes.FORBIDDEN).send();
       }
 
-      await autoExpireHolds(fastify.prisma, request.user);
+      await fastify.prisma.deflection.expire();
 
       let deflections;
       await fastify.prisma.$transaction(async (tx) => {
