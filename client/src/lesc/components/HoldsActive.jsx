ce48ed7a0a448a7099d176c49bab9f1be783c913
Noel <noel_yap@hotmail.com>
2026-02-01 21:14:14 -0800

    Button to extend hold no longer shows after user arrives.

--- client/src/lesc/components/HoldsActive.jsx
+++ client/src/lesc/components/HoldsActive.jsx
@@ -1,12 +1,13 @@
-import { useNavigate } from 'react-router';
-import { Box, Button, Stack, Title, Text, Loader } from '@mantine/core';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import Api from '@/Api';
-import Incident from './Incident';
-import Hold from './Hold';
-import { useToast } from '@/components/ToastContext';
+import { useNavigate } from "react-router";
+import { Box, Button, Stack, Title, Text } from "@mantine/core";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-function HoldsActive ({ incident, deflections, isFetchingDeflections, onCancelHoldClick }) {
+import Api from "@/Api";
+import Incident from "./Incident";
+import Hold from "./Hold";
+import { useToast } from "@/components/ToastContext";
+
+function HoldsActive({ incident, onCancelHoldClick }) {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
   const { showToast } = useToast();
@@ -14,15 +15,19 @@ function HoldsActive ({ incident, deflections, isFetchingDeflections, onCancelHo
   const extendAllHoldsMutation = useMutation({
     mutationFn: () => Api.incidents.extend(incident.id),
     onSuccess: (response) => {
-      queryClient.setQueryData(['deflections', incident?.id, 'active'], response.data);
-      showToast('All active holds have been reset to 60 minutes.', 'success');
+      queryClient.setQueryData(
+        ["deflections", incident?.id, "active"],
+        response.data,
+      );
+      showToast("All active holds have been reset to 60 minutes.", "success");
     },
     onError: () => {
-      showToast('Couldn’t extend holds. Please try again.', 'error');
+      showToast("Couldn’t extend holds. Please try again.", "error");
     },
   });
 
-  function onExtendAllClick () {
+  function onExtendAllClick() {
+    console.log(incident);
     extendAllHoldsMutation.mutate();
   }
 
@@ -36,34 +41,40 @@ function HoldsActive ({ incident, deflections, isFetchingDeflections, onCancelHo
       )}
       {!isFetchingDeflections && (!deflections || deflections.length === 0) && (
         <>
-          <Box bdrs='50%' bg='gray.1' w='160px' h='160px' mx='auto' />
-          <Box align='center'>
+          <Box bdrs="50%" bg="gray.1" w="160px" h="160px" mx="auto" />
+          <Box align="center">
             <Title order={4}>You don't have any active holds</Title>
-            <Text size='md' c='dimmed'>New holds will show up here once you start them.</Text>
+            <Text size="md" c="dimmed">
+              New holds will show up here once you start them.
+            </Text>
           </Box>
         </>
       )}
       {!isFetchingDeflections && deflections && deflections.length > 0 && (
         <>
-          <Stack gap='md'>
+          <Stack gap="md">
             {deflections?.map((deflection) => (
               <Hold
                 key={deflection.id}
                 deflection={deflection}
                 onCancelClick={() => onCancelHoldClick(deflection)}
                 onDetailsClick={() => {
-                  navigate(deflection.subjectId ? `/holds/${deflection.id}` : `/holds/${deflection.id}/subject`);
+                  navigate(
+                    deflection.subjectId
+                      ? `/holds/${deflection.id}`
+                      : `/holds/${deflection.id}/subject`,
+                  );
                 }}
               />
             ))}
           </Stack>
-          <Button
-            variant='secondary'
-            fullWidth
-            onClick={onExtendAllClick}
-          >
-            Extend all holds
-          </Button>
+          {incident[0].arrivedAt != null ? (
+            ""
+          ) : (
+            <Button variant="secondary" fullWidth onClick={onExtendAllClick}>
+              Extend all holds
+            </Button>
+          )}
         </>
       )}
     </>
