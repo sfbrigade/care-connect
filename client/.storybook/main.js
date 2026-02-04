639a47fca701bb8d77b4394b717c9193e6c688f0
Graham Place <grahamgplace@gmail.com>
2026-02-04 15:38:41 -0800

    [#256] Toast Improvements
    * Add Toast stories
    
    * Toast on top, not bottom
    
    * Toast styles to match figma, unify global color constants
    
    * Uninstall storybook onboarding add on
    
    * Support user dismissing toast notifications
    
    * Add slideDown css to app to fix toast animation
    
    * Add useMobile hook to place toasts differently on mobile vs desktop
    
    * Update toast story description
    
    * Fix lint
    
    * Refactor to use Mantine Notification component and theme colors
    
    ---------
    
    Co-authored-by: Francis Li <francisli@users.noreply.github.com>
    Co-authored-by: Francis Li <mail@francisli.com>

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -20,7 +20,6 @@ const config = {
   addons: [
     getAbsolutePath('@chromatic-com/storybook'),
     getAbsolutePath('@storybook/addon-docs'),
-    getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-a11y'),
     getAbsolutePath('@storybook/addon-vitest'),
     getAbsolutePath('@vueless/storybook-dark-mode')
