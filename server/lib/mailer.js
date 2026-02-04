debabd3e4b0714e9e157d6044407c078199f8a34
beaudrykock-moi <beaudry.kock@sfgov.org>
2026-02-04 14:30:13 -0800

    Fixing preview mail setting causing CI error

--- server/lib/mailer.js
+++ server/lib/mailer.js
@@ -42,7 +42,7 @@ const options = {
 
 let mailer;
 
-export function configureMailer (lib) {
+export function configureMailer(lib) {
   const transport = lib.createTransport(options);
 
   mailer = new Email({
@@ -50,6 +50,7 @@ export function configureMailer (lib) {
       from: process.env.SMTP_FROM_EMAIL_ADDRESS,
     },
     send: true,
+    preview: false,
     transport,
     views: {
       options: {
@@ -62,7 +63,7 @@ export function configureMailer (lib) {
 
 configureMailer(nodemailer);
 
-async function send (options) {
+async function send(options) {
   if (process.env.SMTP_ENABLED !== 'true') {
     return;
   }
