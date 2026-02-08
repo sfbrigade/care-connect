d2d88930d2047fd31acdecc051d596dfefa36b78
Francis Li <mail@francisli.com>
2026-02-07 16:08:30 -0800

    Add Position Details

--- client/src/UserProfile/UserProfilePage.jsx
+++ client/src/UserProfile/UserProfilePage.jsx
@@ -1,9 +1,10 @@
 import { NavLink } from 'react-router';
-import { useAuthContext } from '@/AuthContext';
-import { IconArrowLeft } from '@tabler/icons-react';
+import { Anchor, Box, Container, Divider, Group, Stack, Text, Title } from '@mantine/core';
+import { IconArrowLeft, IconPencilMinus } from '@tabler/icons-react';
 import { Head } from '@unhead/react';
+
+import { useAuthContext } from '@/AuthContext';
 import Header from '@/components/Header';
-import { Anchor, Box, Card, Group, Text } from '@mantine/core';
 import IconButtonLink from '@/components/IconButtonLink';
 
 function UserProfilePage () {
@@ -19,40 +20,71 @@ function UserProfilePage () {
           <IconButtonLink icon={IconArrowLeft} to='/' />
         </Group>
       </Header>
-      <Card px='xl' py='0'>
-        {user && (
-          <>
-            <Box>
-              <Text size='32px'>{user.firstName} {user.lastName}</Text>
-              <Text c='gray.6' size='14px'>{user.title}</Text>
-            </Box>
-
-            <Text mt='2xl' size='24px'>Personal Information</Text>
-
-            <Box my='md'>
-              <Text size='md' c='gray.6'>Name</Text>
-              <Text size='md'>{user.firstName} {user.lastName}</Text>
-            </Box>
-
-            <Box>
-              <Text size='md' c='gray.6'>Email Address</Text>
-              <Text size='md'>{user.email}</Text>
-            </Box>
-
-            <Text mt='xl' size='14px' ta='center' c='gray.5'>
-              For assistance with profile updates, please contact careconnect@sfgov.org
-            </Text>
-          </>
-        )}
-        {!user && (
-          <>
-            <Text size='xl'>Please log in to view this page</Text>
-            <Anchor component={NavLink} to='/login'>
-              Log in
-            </Anchor>
-          </>
-        )}
-      </Card>
+      <Container>
+        <Stack>
+          {user && (
+            <>
+              <Box>
+                <Title order={2}>{user.firstName} {user.lastName}</Title>
+                {user.unit && <Text c='gray.6' size='sm'>{user.unit?.name}</Text>}
+              </Box>
+              <Stack gap='sm'>
+                <Title order={3}>Personal Information</Title>
+                <Box>
+                  <Text size='md' c='gray.6'>Name</Text>
+                  <Text size='md'>{user.firstName} {user.lastName}</Text>
+                </Box>
+                <Box>
+                  <Text size='md' c='gray.6'>Email Address</Text>
+                  <Text size='md'>{user.email}</Text>
+                </Box>
+              </Stack>
+              {(user.organizationId === 'sfpd' || user.organizationId === 'sfso') && (
+                <>
+                  <Divider />
+                  <Stack gap='sm'>
+                    <Group justify='space-between'>
+                      <Title order={3}>Position details</Title>
+                      <IconButtonLink icon={IconPencilMinus} to='/profile/edit' />
+                    </Group>
+                    <Box>
+                      <Text size='md' c='gray.6'>Star number</Text>
+                      <Text size='md'>{user.badgeNumber}</Text>
+                    </Box>
+                    <Box>
+                      <Text size='md' c='gray.6'>Unit</Text>
+                      <Text size='md'>{user.unit?.name}</Text>
+                    </Box>
+                    {user.organizationId === 'sfso' && (
+                      <>
+                        <Box>
+                          <Text size='md' c='gray.6'>Rank</Text>
+                          <Text size='md'>{user.title?.name}</Text>
+                        </Box>
+                        <Box>
+                          <Text size='md' c='gray.6'>Prop 115 certification</Text>
+                          <Text size='md'>{user.prop115Certified ? 'Yes' : 'No'}</Text>
+                        </Box>
+                      </>
+                    )}
+                  </Stack>
+                </>
+              )}
+              <Text size='sm' ta='center' c='gray.5'>
+                For assistance with profile updates, please contact <Anchor href='mailto:careconnect@sfgov.org'>careconnect@sfgov.org</Anchor>
+              </Text>
+            </>
+          )}
+          {!user && (
+            <>
+              <Text size='xl'>Please log in to view this page</Text>
+              <Anchor component={NavLink} to='/login'>
+                Log in
+              </Anchor>
+            </>
+          )}
+        </Stack>
+      </Container>
     </>
   );
 }
