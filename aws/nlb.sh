3b8d52dfbc20a3230f35113a6817c2ac62748280
Francis Li <mail@francisli.com>
2026-02-25 14:53:04 -0800

    Remove commented code, export alb target group arn

--- aws/nlb.sh
+++ aws/nlb.sh
@@ -6,16 +6,3 @@ aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM --stack-name
 
 # wait for completion
 aws cloudformation wait stack-create-complete --stack-name ${BASE_NAME}-nlb --output text
-
-# # output the bucket name
-# echo "Bucket name:"
-# aws cloudformation describe-stacks --stack-name ${BASE_NAME}-s3 --query 'Stacks[0].Outputs[?OutputKey==`BucketName`].OutputValue' --output text
-
-# # output the access key id
-# echo "Access Key ID:"
-# aws cloudformation describe-stacks --stack-name ${BASE_NAME}-s3 --query 'Stacks[0].Outputs[?OutputKey==`BucketUserAccessKeyId`].OutputValue' --output text
-
-# # output the secret access key
-# SECRET_ARN=`aws cloudformation describe-stacks --stack-name ${BASE_NAME}-s3 --query 'Stacks[0].Outputs[?OutputKey==\`BucketUserSecretAccessKeyARN\`].OutputValue' --output text`
-# echo "Secret Access Key:"
-# aws secretsmanager get-secret-value --query 'SecretString' --output text --secret-id $SECRET_ARN
