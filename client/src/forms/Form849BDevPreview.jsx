8b000f93aa21983b6fa1228bacba058161b4673e
John Dunning <fw@johndunning.com>
2026-03-01 15:06:57 -0800

    Add /forms/849b-dev preview route
    Clean up text in CertificateOfRelease849BForm.jsx.

---
+++ client/src/forms/Form849BDevPreview.jsx
@@ -0,0 +1,71 @@
+import { PDFViewer } from '@react-pdf/renderer';
+import { useParams } from 'react-router';
+import { useQuery } from '@tanstack/react-query';
+import { Container, Group, Loader, Alert } from '@mantine/core';
+
+import Api from '@/Api';
+import CertificateOfRelease849BForm from '../../../server/lib/forms/CertificateOfRelease849BForm.jsx';
+
+function buildFormData (deflection) {
+  const subject = deflection?.subject;
+  const subjectName = subject
+    ? [subject.firstName, subject.middleInitial, subject.lastName].filter(Boolean).join(' ')
+    : '';
+
+  const deputy = deflection?.releasedBy || deflection?.createdBy;
+  const deputyTitle = deputy?.title?.name || '';
+  const deputyName = deputy ? `${deputy.firstName} ${deputy.lastName}` : '';
+  const deputyBadge = deputy?.badgeNumber || '';
+  const deputyRankNameStar = [deputyTitle, deputyName, deputyBadge ? `#${deputyBadge}` : '']
+    .filter(Boolean)
+    .join(' ');
+
+  const unitIdentifier =
+    deflection?.incident?.createdByUnit?.name || deputy?.unit?.name || '';
+
+  return {
+    subjectName,
+    detentionDate: deflection?.createdAt || null,
+    releaseDate: deflection?.releasedAt || null,
+    deputyRankNameStar,
+    unitIdentifier,
+  };
+}
+
+export default function Form849BDevPreview () {
+  const { deflectionId } = useParams();
+
+  const { data: deflection, isLoading, error } = useQuery({
+    queryKey: ['deflections', deflectionId],
+    queryFn: () => Api.deflections.get(deflectionId).then((r) => r.data),
+    enabled: !!deflectionId,
+  });
+
+  if (isLoading) {
+    return (
+      <Container py='xl'>
+        <Group justify='center'>
+          <Loader />
+        </Group>
+      </Container>
+    );
+  }
+
+  if (error) {
+    return (
+      <Container py='xl'>
+        <Alert color='red' title='Error'>
+          Could not load deflection #{deflectionId}
+        </Alert>
+      </Container>
+    );
+  }
+
+  const data = buildFormData(deflection ?? {});
+
+  return (
+    <PDFViewer style={{ width: '100%', height: '100vh', border: 'none' }}>
+      <CertificateOfRelease849BForm data={data} />
+    </PDFViewer>
+  );
+}
