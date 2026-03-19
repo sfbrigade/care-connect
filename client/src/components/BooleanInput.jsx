2b7b04e88a6856ddfe100867a4357e506d1fe4ca
beaudrykock-moi <beaudry.kock@sfgov.org>
2026-03-19 11:23:00 -0700

    [Closes #394] Adds notification to IS flow to remind SD about property return (#451)
    * Adds notification to IS flow to remind SD about property return (#394)
    
    * Remove unnecessary local storage state
    
    * Use BooleanInput component
    
    * Replace checkbox with BooleanInput
    
    * Restore requirement of property return
    
    * Update and fix tests
    
    ---------
    
    Co-authored-by: Francis Li <francisli@users.noreply.github.com>
    Co-authored-by: Francis Li <mail@francisli.com>

--- client/src/components/BooleanInput.jsx
+++ client/src/components/BooleanInput.jsx
@@ -2,7 +2,7 @@ import { Chip, Group, Input } from '@mantine/core';
 import { useUncontrolled } from '@mantine/hooks';
 import { useTranslation } from 'react-i18next';
 
-function BooleanInput ({ label, value, defaultValue, onChange, ...props }) {
+function BooleanInput ({ label, description, required, disabled, value, children, defaultValue, onChange, ...props }) {
   const { t } = useTranslation();
   const [_value, setValue] = useUncontrolled({
     value: (typeof value === 'boolean') ? (value ? 'true' : 'false') : value,
@@ -13,15 +13,16 @@ function BooleanInput ({ label, value, defaultValue, onChange, ...props }) {
   });
 
   return (
-    <Input.Wrapper label={label}>
+    <Input.Wrapper label={label} description={description} required={required} disabled={disabled}>
+      {children}
       <Chip.Group
         {...props}
         value={_value}
         onChange={setValue}
       >
         <Group gap='sm' mt='md'>
-          <Chip value='true'>{t('ternary.YES')}</Chip>
-          <Chip value='false'>{t('ternary.NO')}</Chip>
+          <Chip value='true' disabled={disabled}>{t('ternary.YES')}</Chip>
+          <Chip value='false' disabled={disabled}>{t('ternary.NO')}</Chip>
         </Group>
       </Chip.Group>
     </Input.Wrapper>
