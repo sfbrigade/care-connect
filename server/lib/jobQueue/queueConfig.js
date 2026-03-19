10dd32ab5c7520a96319377a59465976bd2a18d8
Graham Place <grahamgplace@gmail.com>
2026-03-19 14:37:22 -0700

    Configure pg-boss queue for invite job

--- server/lib/jobQueue/queueConfig.js
+++ server/lib/jobQueue/queueConfig.js
@@ -1,4 +1,13 @@
-// TODO: add queue handlers here
-const queues = [];
+import inviteEmail from '../jobs/inviteEmail.js';
+import { QUEUE_INVITE_EMAIL } from './queueNames.js';
+
+const queues = [
+  {
+    name: QUEUE_INVITE_EMAIL,
+    options: { retryLimit: 3, retryBackoff: true },
+    handler: async (job) => inviteEmail(job.data),
+    deadLetterData: (data) => ({ inviteId: data?.inviteId }),
+  },
+];
 
 export default queues;
--- server/lib/jobQueue/queueNames.js
+++ server/lib/jobQueue/queueNames.js
@@ -1,1 +1,2 @@
 // TODO: Define queue names outside of the queue handler config to avoid importing handler code in producers (route handlers)
+export const QUEUE_INVITE_EMAIL = 'invite-email';
