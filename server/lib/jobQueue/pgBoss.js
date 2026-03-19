852fb7f0ebfaac6bb37a05aa70dcd1cb374b972f
Graham Place <grahamgplace@gmail.com>
2026-03-19 14:21:17 -0700

    Add pg-boss based queue to lib

---
+++ server/lib/jobQueue/pgBoss.js
@@ -0,0 +1,7 @@
+import { PgBoss } from 'pg-boss';
+
+export function createBoss () {
+  return new PgBoss({
+    connectionString: process.env.DATABASE_URL,
+  });
+}
---
+++ server/lib/jobQueue/queueConfig.js
@@ -0,0 +1,4 @@
+// TODO: add queue handlers here
+const queues = [];
+
+export default queues;
