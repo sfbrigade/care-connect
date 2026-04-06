1a04a8984c0a0085bca054f0954718194a1609ac
Aaron Hans <aaronhans@gmail.com>
2026-04-05 21:05:16 -0700

    fix 2nd page form generation

--- server/lib/forms/pdf/FormCoR.jsx
+++ server/lib/forms/pdf/FormCoR.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
 import { z } from 'zod';
 import { readFile } from 'fs/promises';
 import { join } from 'path';
-import { PDFDocument } from 'pdf-lib';
 import { fillCoR } from './fillCoR.js';
 import { formatDateParts, formatTime, formatDateOnly } from '../formUtils.js';
-import FormNarcoticsNotice from '../jsx/FormNarcoticsNotice.jsx';
 
 export const metadata = {
   generatorType: 'pdf',
@@ -123,34 +120,7 @@ export const metadata = {
       signature: `${deflectionData.deputyName} #${deflectionData.deputyBadge}`,
     };
 
-    const certBytes = await fillCoR(templateBytes, formData);
-
-    // If narcotics or paraphernalia were seized, append the narcotics notice as page 2
-    if (deflectionData.narcoticsSubstance || deflectionData.narcoticsParaphernalia) {
-      const { renderFormToHtml, renderToPdf } = await import('#lib/pdf.js');
-
-      const noticeData = {
-        date: deflectionData.releaseDateFormatted,
-        cadNumber: deflectionData.cadNumber,
-        substanceSeized: deflectionData.narcoticsSubstance === true,
-        paraphernaliaSeized: deflectionData.narcoticsParaphernalia === true,
-      };
-
-      const noticeHtml = await renderFormToHtml(FormNarcoticsNotice, noticeData, { title: 'Narcotics Notice' });
-      const noticeBytes = await renderToPdf(noticeHtml);
-
-      // Merge: cert page 1 + narcotics notice page(s)
-      const certDoc = await PDFDocument.load(certBytes);
-      const noticeDoc = await PDFDocument.load(noticeBytes);
-      const copiedPages = await certDoc.copyPages(noticeDoc, noticeDoc.getPageIndices());
-      for (const page of copiedPages) {
-        certDoc.addPage(page);
-      }
-
-      return certDoc.save();
-    }
-
-    return certBytes;
+    return fillCoR(templateBytes, formData);
   },
 };
 
