387b51b3ec6fd24c1cd2d4a93fd4c569f75a7efd
Saad Abdali <saadabdali@gmail.com>
2026-04-13 23:18:49 -0700

    Is the photo clear enough to use?

--- client/src/components/IdScanner.jsx
+++ client/src/components/IdScanner.jsx
@@ -211,6 +211,8 @@ function IdScanner ({ opened, onResult, onClose }) {
         )}
 
         {capturedImage && !processing && (
+          <Stack gap='xs' align='center'>
+          <Text c='gray.5' size='sm'>Is the photo clear enough to use?</Text>
           <Group grow w='100%'>
             <Button
               variant='light'
@@ -224,6 +226,7 @@ function IdScanner ({ opened, onResult, onClose }) {
               Use photo
             </Button>
           </Group>
+          </Stack>
         )}
       </div>
 
