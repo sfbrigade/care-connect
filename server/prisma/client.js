379a742bb5a819eb134f630bd9c1cf1d13421e0d
Aaron Hans <aaronhans@gmail.com>
2026-04-25 16:15:42 -0700

    less strict update because we do not change keys to prevent deadlock

--- server/prisma/client.js
+++ server/prisma/client.js
@@ -14,7 +14,7 @@ const prisma = new PrismaClient({
   model: {
     facility: {
       async findByIdForUpdate (tx, id) {
-        const result = await tx.$queryRaw`SELECT * FROM "Facility" WHERE "id" = ${id}::uuid FOR UPDATE`;
+        const result = await tx.$queryRaw`SELECT * FROM "Facility" WHERE "id" = ${id}::uuid FOR NO KEY UPDATE`;
         return result.length > 0 ? result[0] : null;
       }
     },
@@ -22,10 +22,10 @@ const prisma = new PrismaClient({
       async findByIdForUpdate (tx, id) {
         let result;
         if (Array.isArray(id)) {
-          result = await tx.$queryRaw`SELECT * FROM "BedType" WHERE "id" = ANY(${id}::uuid[]) FOR UPDATE`;
+          result = await tx.$queryRaw`SELECT * FROM "BedType" WHERE "id" = ANY(${id}::uuid[]) FOR NO KEY UPDATE`;
           return result;
         } else {
-          result = await tx.$queryRaw`SELECT * FROM "BedType" WHERE "id" = ${id}::uuid FOR UPDATE`;
+          result = await tx.$queryRaw`SELECT * FROM "BedType" WHERE "id" = ${id}::uuid FOR NO KEY UPDATE`;
           return result.length > 0 ? result[0] : null;
         }
       }
@@ -34,10 +34,10 @@ const prisma = new PrismaClient({
       async findByIdForUpdate (tx, id) {
         let result;
         if (Array.isArray(id)) {
-          result = await tx.$queryRaw`SELECT * FROM "Deflection" WHERE "id" = ANY(${id}::int[]) ORDER BY "id" FOR UPDATE`;
+          result = await tx.$queryRaw`SELECT * FROM "Deflection" WHERE "id" = ANY(${id}::int[]) ORDER BY "id" FOR NO KEY UPDATE`;
           return result;
         }
-        result = await tx.$queryRaw`SELECT * FROM "Deflection" WHERE "id" = ${id} FOR UPDATE`;
+        result = await tx.$queryRaw`SELECT * FROM "Deflection" WHERE "id" = ${id} FOR NO KEY UPDATE`;
         return result.length > 0 ? result[0] : null;
       },
       async expire (now = new Date()) {
