78192ae939b7c979108dad3da59a49bbdd842399
Saad Abdali <saadabdali@gmail.com>
2026-04-28 09:22:48 -0700

    Remove redundant pre-lock read in exit-to-jail

--- server/routes/api/deflections/exit-to-jail.js
+++ server/routes/api/deflections/exit-to-jail.js
@@ -79,10 +79,6 @@ export default async function (fastify, opts) {
         return reply.code(StatusCodes.NOT_FOUND).send();
       }
 
-      if (!EXIT_TO_JAIL_ELIGIBLE_STATUSES.has(deflection.subjectStatus)) {
-        return reply.code(StatusCodes.CONFLICT).send();
-      }
-
       try {
         await fastify.prisma.$transaction(async (tx) => {
           const { bedTypeId } = deflection;
