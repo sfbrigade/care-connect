e150f78119d60f82e280a289664c0c4fe3755698
Aaron Hans <aaronhans@gmail.com>
2026-04-29 11:58:14 -0700

    retreat from dynamic fastify compress because doesn't play well with streaming ssr or something

--- server/plugins/compress.js
+++
@@ -1,9 +0,0 @@
-import fp from 'fastify-plugin';
-import fastifyCompress from '@fastify/compress';
-
-export default fp(async (fastify) => {
-  await fastify.register(fastifyCompress, {
-    encodings: ['br', 'gzip'],
-    threshold: 1024,
-  });
-});
