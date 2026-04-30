82319f872bbae1ddd421b7de9d0d5e1fb90aa95c
Graham Place <grahamgplace@gmail.com>
2026-04-29 22:01:12 -0700

    Enable PostHog client-side exception auto-capture

--- client/src/analytics/PosthogProvider.jsx
+++ client/src/analytics/PosthogProvider.jsx
@@ -30,6 +30,7 @@ function PosthogProvider () {
       posthog.init(apiKey, {
         api_host: apiHost || DEFAULT_API_HOST,
         capture_pageview: true,
+        capture_exceptions: true,
         // We can't store ANY PII of people brought to a facility (not of the app users themselves) for >72 hrs,
         // Disable Posthog session recording entirely until we have a way to anonymize or wipe it within the time limit.
         disable_session_recording: true,
