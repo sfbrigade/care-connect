d79db26a7d57f414adf3f668d85e86e85f07ce95
Saad Abdali <saadabdali@gmail.com>
2026-04-29 18:44:21 -0700

    Enlarge font of 'Is photo clear enough' message; move it up a little

--- client/src/components/IdScanner.jsx
+++ client/src/components/IdScanner.jsx
@@ -232,8 +232,8 @@ function IdScanner ({ opened, onResult, onClose }) {
         )}
 
         {capturedImage && !processing && (
-          <Stack gap='lg' align='center'>
-            <Text c='gray.5' size='sm'>Is the photo clear enough to use?</Text>
+          <Stack gap='xl' align='center'>
+            <Text c='white' size='lg' fw={600} ta='center'>Is the photo clear enough to use?</Text>
             <Group grow w='100%'>
               <Button
                 variant='light'
