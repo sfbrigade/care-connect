48a2e050b145de3e2437a2607b48e6ee6a4cf534
Francis Li <mail@francisli.com>
2026-05-01 09:37:58 -0700

    Allow fallback to BASE_URL

--- server/lib/mailer.js
+++ server/lib/mailer.js
@@ -43,7 +43,7 @@ const options = {
 let mailer;
 
 export const EMAIL_TITLE = process.env.EMAIL_SITE_TITLE || 'City and County of San Francisco: CareConnect';
-export const EMAIL_BASE_URL = process.env.EMAIL_BASE_URL || 'https://careconnect.sf.gov';
+export const EMAIL_BASE_URL = process.env.EMAIL_BASE_URL || process.env.BASE_URL || 'https://careconnect.sf.gov';
 
 export function configureMailer (lib) {
   const transport = lib.createTransport(options);
