49660a6222aa3194005a93b831f4dc62debb8616
beaudrykock-moi <beaudry.kock@sfgov.org>
2026-05-03 18:05:07 -0700

    [Closes #847] Makes 849b org-generic (#848)
    Co-authored-by: beaudrykock <beaudry.kock@mac.com>

--- server/lib/forms/849b/releaseNarrative.js
+++ server/lib/forms/849b/releaseNarrative.js
@@ -18,7 +18,7 @@ export function build849bReleaseNarrative ({ caseNumber, cadNumber, behavior } =
     `Cad number: ${normalizedCadNumber}`,
     'Subject was brought to RESET because they were found to be under the influence of a controlled substance or alcohol in a public location. Upon being able to care for themselves, they were released from their detention.',
     '',
-    'The SFPD Officer who brought the person to RESET recorded the following observations on the 647(f) documentation:',
+    'The Officer who brought the person to RESET recorded the following observations on the 647(f) documentation:',
     behaviorNarrative,
   ].join('\n');
 }
