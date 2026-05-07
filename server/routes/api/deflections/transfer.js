69a3f851d3eabf04b6ea7c282715305326c8a08b
beaudrykock-moi <beaudry.kock@sfgov.org>
2026-05-07 14:37:13 -0700

    Removes sfso incident email (#928) (#932)
    Co-authored-by: Aaron Hans <aaronhans@gmail.com>

--- server/routes/api/deflections/transfer.js
+++ server/routes/api/deflections/transfer.js
@@ -142,7 +142,6 @@ export default async function (fastify, opts) {
         recipientEmail: [
           'SFPD.Data.Transfer.Authorized@sfgov.org',
           'Andrew.bley@sfgov.org',
-          'Sfso-incidentreports@sfgov.org',
         ],
       });
 
